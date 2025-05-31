e4a057f895d69826843b292bc5742caa00805415
AKASH M <87299311+gamkers@users.noreply.github.com>
2025-05-31 08:59:57 +0530

    Updates

---
+++ assets/browser-BWevJGZV.js
@@ -0,0 +1,1 @@
+import{g as e}from"./index-m1tq8ryj.js";var o=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")};const r=e(o),s=Object.freeze(Object.defineProperty({__proto__:null,default:r},Symbol.toStringTag,{value:"Module"}));export{s as b};
